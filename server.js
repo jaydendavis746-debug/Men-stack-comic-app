44f5eb0d23d0bad731d5ae38791cc7cd0a8fa4e4
Jayden Davis <jaydendavis746@gmail.com>
2026-01-22 03:22:17 +0000

    Very lukewarm styling

--- server.js
+++ server.js
@@ -2,19 +2,25 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
-const app = express()
+
 import mongoose from 'mongoose'
 import methodOverride from 'method-override'
 import session from 'express-session'
 import morgan from 'morgan'
 import isSignedIn from './middleware/is-signed-in.js'
 import passUserToView from './middleware/pass-user-to-views.js'
-
+import path from 'path'
+import { fileURLToPath } from 'url';
+const app = express()
 import authController from './controllers/auth.js'
 import comicsController from './controllers/comics.js'
 import genresController from './controllers/genres.js'
 
 import Comic from './models/comics.js'
+
+const _dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.join(_dirname, 'public')));
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -23,6 +29,8 @@ mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
+
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
@@ -40,6 +48,7 @@ app.get('/', async (req, res)=>{
 const comics = await Comic.find().sort({ likedByUsers: "desc" }).limit(3);
 res.render('index.ejs', {user: req.session.user, comics})
 })
+
 
 app.use('/auth', authController);
 app.use(isSignedIn);
