942e03a3fdf7c20ed6cb112e0559b09867b4377a
Jayden Davis <jaydendavis746@gmail.com>
2026-01-23 10:27:12 +0000

    Better styling

--- models/requests.js
+++ models/requests.js
@@ -4,6 +4,16 @@ const requestSchema = mongooose.Schema({
 
     requestType: {
         type: String,
-        
-    }
+        required: true,
+    },
+
+    description:{
+        type: String,
+        required: true,
+    },
+
+    publisher:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
 })
