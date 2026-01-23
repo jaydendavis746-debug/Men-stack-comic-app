bf97aaff0db3fec2eb32d7262dccd165813ce178
Jayden Davis <jaydendavis746@gmail.com>
2026-01-23 11:00:51 +0000

    Request page added

--- controllers/requests.js
+++ controllers/requests.js
@@ -2,10 +2,18 @@ import express from 'express'
 const router = express.Router()
 import Request from '../models/requests.js'
 import User from '../models/users.js'
+import { request } from 'http'
 
 
 router.get('/', (req, res) => {
     res.render('requests/index.ejs')
+})
+
+
+router.post('/', async (req, res)=>{
+    req.body.publisher = req.session.user._id
+    await Request.create(req.body)
+    res.redirect('/')
 })
 
 
